b39560e0aa857a3301e91cc4ee4d2306d64051ed
joao <joao.silverio@uft.lu>
2020-12-21 23:23:56 +0100

    implemented navigation in react

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,16 +1,158 @@
 import React from 'react';
-import {BrowserRouter as Router, Route,Switch} from "react-router-dom";
+import {BrowserRouter as Router, Route,Switch, useRouteMatch} from "react-router-dom";
 import './App.css';
 import HomePage from "./pages/HomePage";
 import BasePage from "./pages/BasePage";
-
+import {Alignment, Button, Navbar} from "@blueprintjs/core";
+import {Routes} from "./config/routes";
+function DashboardNavigation(){
+    const match = useRouteMatch();
+    return (
+        <Switch>
+            <Route path={`${match.path}${Routes.CATALOG}`}>
+                <BasePage>
+                    <div>{"Catalog"}</div>
+                </BasePage>
+            </Route>
+            <Route path={`${match.path}${Routes.DATA_VISUALIZATION}`}>
+                <BasePage>
+                    <div>{"Maps and Graphs"}</div>
+                </BasePage>
+            </Route>
+            <Route path={`${match.path}${Routes.SIGHTINGS_HISTORY}`}>
+                <BasePage>
+                    <div>{"Sightings"}</div>
+                </BasePage>
+            </Route>
+            <Route path={`${match.path}${Routes.SIGHTINGS_ADD}`}>
+                <BasePage>
+                    <div>{"Add Sighting"}</div>
+                </BasePage>
+            </Route>
+            <Route path={`${match.path}${Routes.BAYWATCH_SCAN}`}>
+                <BasePage>
+                    <div>{"Baywatch Scan"}</div>
+                </BasePage>
+            </Route>
+            <Route path={`${match.path}${Routes.BAYWATCH_DATA_UPLOAD}`}>
+                <BasePage>
+                    <div>{"Data Upload"}</div>
+                </BasePage>
+            </Route>
+            <Route path={`${match.path}${Routes.BAYWATCH_HISTORY}`}>
+                <BasePage>
+                    <div>{"Maps and Graphs"}</div>
+                </BasePage>
+            </Route>
+            <Route path={`${match.path}${Routes.BACKOFFICE}`}>
+               <BackOfficeDashboardNavigation />
+            </Route>
+            <Route path={match.path}>
+                <BasePage>
+                    <div>{"Dashboard"}</div>
+                </BasePage>
+            </Route>
+        </Switch>
+    )
+}
+function BackOfficeDashboardNavigation(){
+    const match = useRouteMatch();
+    return (
+        <Switch>
+            <Route path={`${match.path}${Routes.MANAGEMENT_COLABORATORS}`}>
+                <BasePage>
+                    <div>{"Colaborators and Boats"}</div>
+                </BasePage>
+            </Route>
+            <Route path={`${match.path}${Routes.MANAGEMENT_BAYWATCH}`}>
+                <BasePage>
+                    <div>{"BayWatch"}</div>
+                </BasePage>
+            </Route>
+            <Route path={`${match.path}${Routes.AUDIT_LOG}`}>
+                <BasePage>
+                    <div>{"Audit Log"}</div>
+                </BasePage>
+            </Route>
+            <Route path={`${match.path}${Routes.MANAGEMENT_PUBLICATIONS}`}>
+                <BasePage>
+                    <div>{"Publications"}</div>
+                </BasePage>
+            </Route>
+            <Route path={`${match.path}${Routes.NEWS}`}>
+                <BasePage>
+                    <div>{"News Managment"}</div>
+                </BasePage>
+            </Route>
+            <Route path={match.path}>
+                <BasePage>
+                    <div>{"Backend Dashboard"}</div>
+                </BasePage>
+            </Route>
+        </Switch>
+    )
+}
+function NavigationBar(){
+    return(
+        <Navbar>
+            <Navbar.Group align={Alignment.LEFT}>
+                <Navbar.Heading>Monicet</Navbar.Heading>
+                <Navbar.Divider />
+                <Button
+                    onClick={()=> window.location.href = Routes.NEWS}
+                    className="bp3-minimal"
+                    icon="clipboard"
+                    text="News"  />
+                <Button
+                    onClick={()=> window.location.href = Routes.TEAM}
+                    className="bp3-minimal"
+                    icon="people"
+                    text="Team And Partners" />
+                <Button
+                    onClick={()=> window.location.href = Routes.PUBLICATIONS}
+                    className="bp3-minimal"
+                    icon="document"
+                    text="Publications" />
+                <Button
+                    onClick={()=> window.location.href = Routes.LOGIN}
+                    className="bp3-minimal"
+                    icon="lock"
+                    text="Login" />
+            </Navbar.Group>
+        </Navbar>
+    )
+}
 function App() {
   return (
     <div className="App">
+            <NavigationBar />
         <Router>
      <BasePage>
          <Switch>
-             <Route path={"/"} >
+             <Route path={Routes.NEWS}>
+                 <BasePage>
+                     <div>{"News Page"}</div>
+                 </BasePage>
+             </Route>
+             <Route path={Routes.TEAM}>
+                 <BasePage>
+                     <div>{"Team and Partner"}</div>
+                 </BasePage>
+             </Route>
+             <Route path={Routes.PUBLICATIONS}>
+                 <BasePage>
+                     <div>{"Publications"}</div>
+                 </BasePage>
+             </Route>
+             <Route path={Routes.LOGIN}>
+                 <BasePage>
+                     <div>{"Login"}</div>
+                 </BasePage>
+             </Route>
+             <Route path={Routes.DASHBOARD}>
+                <DashboardNavigation />
+             </Route>
+             <Route  path={"/"} >
                  <HomePage />
              </Route>
          </Switch>
@@ -19,5 +161,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
--- ui/src/config/routes.ts
+++ ui/src/config/routes.ts
@@ -1,9 +1,27 @@
 export enum Routes {
+// PUBLIC ROUTES
     HOME= "/",
     NEWS= "/news",
     TEAM="/team",
     PUBLICATIONS="/publications",
     CONTACTS="/contacts",
+    LOGIN="/login",
+// LOGGED IN ROUTES
+    DASHBOARD="/dashboard",
+    CATALOG="/catalog",
+    DATA_VISUALIZATION="/data-visualization",
+    SIGHTINGS_HISTORY="/sightings-history",
+    SIGHTINGS_ADD="/sightings-add",
+    BAYWATCH_SCAN="/baywatch-scan",
+    BAYWATCH_DATA_UPLOAD="/baywatch-upload",
+    BAYWATCH_HISTORY="/baywatch-history",
+//  BACKOFFICE ROUTES
+    BACKOFFICE="/backoffice",
+    MANAGEMENT_COLABORATORS="/colaborators",
+    MANAGEMENT_BAYWATCH="/baywatch",
+    MANAGEMENT_PUBLICATIONS="/publications",
+    AUDIT_LOG="/audit-log",
+
 
 
 
