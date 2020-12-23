ea7d3d53095b978ca58919dc9c6bf2ecdcf30359
Joao Silverio <joao.r.silverio@gmail.com>
2020-12-24 00:18:14 +0100

    fixed security constants

--- src/main/java/pt/geniusgrow/monicet/security/SecurityConstants.java
+++ src/main/java/pt/geniusgrow/monicet/security/SecurityConstants.java
@@ -1,7 +1,11 @@
 package pt.geniusgrow.monicet.security;
 
 import org.springframework.beans.factory.annotation.Value;
-
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+@Configuration
+@PropertySource("classpath:application.properties")
+@PropertySource("classpath:development.properties")
 public class SecurityConstants {
     @Value("${app.api.auth.secret}")
     public static String SECRET;
